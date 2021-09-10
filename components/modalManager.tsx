0c7f22642435016e545a94ec70a2763282894f76
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-10 18:29:03 -0400

    Feat: Start on modal manager

---
+++ components/modalManager.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const ModalManager = (): JSX.Element => {
+  return <Container>test</Container>;
+};
