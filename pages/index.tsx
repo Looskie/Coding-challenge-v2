6091bf195b289c1ce6148b0a6415ab15b1c6239d
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-10 18:31:01 -0400

    Feat: Open modal button for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { useAtom } from "jotai";
 import type { NextPage } from "next";
 import styled from "styled-components";
+import { activeModal } from "../state/modal";
 
 const Home: NextPage = () => {
+  const [modal, setModal] = useAtom(activeModal);
   return (
     <div>
+      <button onClick={() => setModal("addReview")}>Open modal</button>
       <h1>hello</h1>
     </div>
   );
