79bd4dd99013cd8fbe372e96b36c58b5279ce839
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-10 18:29:32 -0400

    Feat: Use modal manager + add it to index file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { Provider } from "jotai";
+import { ModalManager } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider>
       <Component {...pageProps} />
+      <ModalManager />
     </Provider>
   );
 }
