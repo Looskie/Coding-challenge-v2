3e9b0367ae1cc210d83f451b4be16b9ff46bfd56
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-10 20:11:47 -0400

    Feat: BTN

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-const Btn = styled.button``;
+const Btn = styled.button`
+  display: block;
+`;
 
-export const Button = (): JSX.Element => {
-  return <Btn>test</Btn>;
+export const Button = ({
+  text,
+  onClick,
+}: {
+  text: string;
+  onClick: () => unknown;
+}): JSX.Element => {
+  return <Btn onClick={onClick}>{text}</Btn>;
 };
