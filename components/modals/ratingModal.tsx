f3c943de3921d9f458b62b490e984cb390663ea1
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-10 20:12:04 -0400

    Feat: Base of rating modal down

--- components/modals/ratingModal.tsx
+++ components/modals/ratingModal.tsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
+import { Button } from "..";
+import { Stars } from "../stars";
 
 const Container = styled.div``;
+
+const Subtitle = styled.h1`
+  font-weight: 400;
+  margin: 25px 0;
+  font-size: 1.15em;
+`;
 
 export const RatingModal = (): JSX.Element => {
   return (
     <Container>
-      <h1>test</h1>
+      <Subtitle>Rating</Subtitle>
+      <Stars />
+      <Subtitle>Review</Subtitle>
+      <textarea placeholder="Start typing..."></textarea>
+      <Button text="Submit review" onClick={() => console.log("post")} />
     </Container>
   );
 };
