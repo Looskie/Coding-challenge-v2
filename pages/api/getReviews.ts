26d4eed376e2ffdb90a235dd7630c856cbbf1216
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-12 17:03:44 -0400

    Feat: getReview

---
+++ pages/api/getReviews.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+type Data = {
+  success: boolean;
+  message: string;
+  data?: any;
+};
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+): Promise<void> {
+  try {
+    const take = req.query.take;
+
+    if (take !== undefined && isNaN(Number(take))) {
+      return res.status(400).json({
+        success: false,
+        message: "You have a type error!",
+      });
+    }
+
+    const reviews = await prisma.review.findMany({
+      take: !take ? 10 : Number(take),
+    });
+
+    /* Remove id from request */
+    const filterReviews = reviews.map(({ id, ...reviews }) => reviews);
+
+    res.json({
+      success: true,
+      message: "Successfully gathered reviews!",
+      data: {
+        ...filterReviews,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred whilst fetching review(s)!",
+    });
+  }
+}
