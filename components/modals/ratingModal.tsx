d8c40fc1b1b4e869adfd83ea202347b9970965db
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-13 12:30:48 -0400

    .

--- components/modals/ratingModal.tsx
+++ components/modals/ratingModal.tsx
@@ -1,6 +1,10 @@
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
 import styled from "styled-components";
-import { Button } from "..";
-import { Stars } from "../stars";
+import { Button } from "../";
+import { activeModal } from "../../state/modal";
+import { makeRequest } from "../../utils";
 
 const Container = styled.div``;
 
@@ -17,14 +21,70 @@ const Input = styled.input`
   color: #929292;
 `;
 
+const StarContainer = styled.span<{ filled: boolean }>`
+  display: inline-flex;
+  cursor: pointer;
+  color: ${({ filled }) => (filled ? "#FFCD69" : "#E0E0E0")};
+  transition: all 0.1s ease-in-out;
+`;
+
+const Error = styled.span`
+  color: red;
+`;
+
 export const RatingModal = (): JSX.Element => {
+  const [, setActiveModal] = useAtom(activeModal);
+  const [input, setInput] = useState("");
+  const [starValue, setStarValue] = useState(0);
+  const [error, setError] = useState<string | null>(null);
+  const [hoverValue, setHoverValue] = useState<number | null>(null);
+
   return (
     <Container>
+      {error && <Error>{error}</Error>}
       <Subtitle>Rating</Subtitle>
-      <Stars />
+      {[...Array(5)].map((_, i) => {
+        return (
+          <StarContainer
+            onMouseEnter={() => setHoverValue(i + 1)}
+            onMouseLeave={() => setHoverValue(null)}
+            onClick={() => {
+              const newValue = i + 1;
+              setStarValue(newValue);
+            }}
+            filled={(hoverValue || starValue) > i}
+            key={i}
+          >
+            <FaStar size={25} />
+          </StarContainer>
+        );
+      })}
       <Subtitle>Review</Subtitle>
-      <Input placeholder="Start typing..." />
-      <Button text="Submit review" onClick={() => console.log("post")} />
+      <Input
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Start typing..."
+      />
+      <Button
+        text="Submit review"
+        onClick={async () => {
+          if (input.length <= 0) return setError("You must set a review!");
+
+          const { data, error } = await makeRequest(
+            "/api/createReview",
+            "POST",
+            {
+              message: input,
+              rating: starValue,
+            }
+          );
+
+          if (error) {
+            return setError(error);
+          }
+
+          setActiveModal(null);
+        }}
+      />
     </Container>
   );
 };
