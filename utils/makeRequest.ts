731ec05c5cd152beb4fc322fd7c782869154ac45
Cody Miller <50378828+Looskie@users.noreply.github.com>
2021-09-13 12:31:28 -0400

    Fix

--- utils/makeRequest.ts
+++ utils/makeRequest.ts
@@ -1,18 +1,21 @@
 export const makeRequest = async (
   URL: string,
   method: "POST" | "GET" | "DELETE" | "PATCH",
-  body?: BodyInit | null | undefined
+  body?: any
 ): Promise<{ data: unknown | null; error: string | null }> => {
   const req = await fetch(URL, {
     method,
-    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body && JSON.stringify(body),
   });
 
-  const res = await req.json() as {
+  const res = (await req.json()) as {
     success: boolean;
     message: string;
     data?: unknown;
-  }
+  };
 
   if (!req.ok) {
     return { data: null, error: res.message };
